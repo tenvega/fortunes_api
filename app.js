3f1bf0eac560099acd181333b72a925b2ce4af6a
Enoc <etorra@gmail.com>
2018-03-27 06:50:58 -0400

    added bin folder with www file

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fortunes = require('./data/fortunes');
+
+
+
+const app = express();
+
+app.get('/fortunes',(req, res)=>{
+  res.json(fortunes)
+
+});
+
+module.exports = app;
