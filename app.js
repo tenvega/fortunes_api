661aeca62b13b061c55499b8adb7306b1a55f889
Enoc <etorra@gmail.com>
2018-03-27 10:50:02 -0400

    final/added Put & Delete functionality

--- app.js
+++ app.js
@@ -22,25 +22,56 @@ app.get('/fortunes/random', (req, res) => {
 
 app.get('/fortunes/:id', (req, res) => {
 
-res.json(fortunes.find(f => f.id == req.params.id));
+  res.json(fortunes.find(f => f.id == req.params.id));
 });
 
-app.post('/fortunes', (req, res) =>{
+const writeFortunes = json => {
+fs.writeFile('./data/fortunes.json', JSON.stringify(json), err => console.log(err));
 
-  const {message, lucky_number, spirit_animal} = req.body;
+};
+
+app.post('/fortunes', (req, res) => {
+
+  const {
+    message,
+    lucky_number,
+    spirit_animal
+  } = req.body;
 
   const fortune_ids = fortunes.map(f => f.id);
 
 
-const new_fortunes = fortunes.concat({
-id: (fortune_ids.length > 0 ? Math.max(...fortune_ids): 0) + 1,
-message,
-lucky_number,
-spirit_animal
+  const new_fortunes = fortunes.concat({
+    id: (fortune_ids.length > 0 ? Math.max(...fortune_ids) : 0) + 1,
+    message,
+    lucky_number,
+    spirit_animal
+  });
+
+writeFortunes(new_fortunes);
+
+  res.json(new_fortunes);
 });
 
+app.put('/fortunes/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
 
-fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err));
+  const old_fortune = fortunes.find(f => f.id == id);
+
+
+  ['message', 'lucky_number', 'spirit_animal'].forEach(key =>{
+    if(req.body[key]) old_fortune[key] = req.body[key];
+  });
+
+  writeFortunes(fortunes);
+  res.json(fortunes);
+});
+app.delete('/fortunes/:id', (req, res) =>{
+const { id } = req.params;
+const new_fortunes = fortunes.filter( f => f.id != id);
+writeFortunes(new_fortunes);
 res.json(new_fortunes);
 });
 
