22db1c72a4397b255f37f10ddd1603e0c59302ae
Enoc <etorra@gmail.com>
2018-03-27 07:59:57 -0400

    fs.write working

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
+const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 const fortunes = require('./data/fortunes');
 
 
 
 const app = express();
 
-app.get('/fortunes',(req, res)=>{
+app.use(bodyParser.json());
+
+app.get('/fortunes', (req, res) => {
   res.json(fortunes)
 
 });
 
-app.get('/fortunes/random',(req, res)=>{
- console.log('requesting random fortune');
+app.get('/fortunes/random', (req, res) => {
+  console.log('requesting random fortune');
 
- const random_index = Math.floor(Math.random() * fortunes.length);
-
- const r_fortune = fortunes[random_index];
-
- res.json(r_fortune);
+  res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
 });
+
+app.get('/fortunes/:id', (req, res) => {
+
+res.json(fortunes.find(f => f.id == req.params.id));
+});
+
+app.post('/fortunes', (req, res) =>{
+  console.log(req.body);
+
+  const {message, lucky_number, spirit_animal} = req.body;
+
+  const fortune_ids = fortunes.map(f => f.id);
+
+  const fortune ={
+  id: (fortune_ids.length > 0 ? Math.max(...fortune_ids): 0) + 1,
+  message,
+  lucky_number,
+  spirit_animal
+};
+const new_fortunes = fortunes.concat(fortune);
+
+
+fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err));
+res.json(new_fortunes);
+});
+
 module.exports = app;
